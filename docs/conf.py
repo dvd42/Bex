3c95a07ad0b7ff7b86436ada3d2ebd3df7978751
Toolkit User <dvd42.diegovd0296@gmail.com>
2023-04-04 15:50:08 +0000

    updated documentation

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ copyright = '2022, Diego Velazquez'
 author = 'Diego Velazquez'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.5'
 
 
 # -- General configuration ---------------------------------------------------
@@ -47,8 +47,3 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
